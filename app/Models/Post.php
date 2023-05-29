cfb503b3ce56dd50d26f35bb504bfc9c19b6266b
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-05-29 18:04:54 +0300

    Relations in Laravel part 1

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,4 +21,15 @@ class Post extends Model
 
 
     protected $guarded = [];
+
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
